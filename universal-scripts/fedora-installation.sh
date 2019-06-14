a3bc61e17474dd95166cd3e0076b0a2f9ba75fe6
turtlebasket <turtlebasket@gmail.com>
2019-06-14 14:22:08 -0700

    add more stuff

--- universal-scripts/fedora-installation.sh
+++ universal-scripts/fedora-installation.sh
@@ -3,7 +3,10 @@ sudo dnf copr enable gregw/i3desktop
 sudo dnf copr enable mhartgring/polybar
 
 # grab me some essential packages
-sudo dnf install i3-gaps polybar rofi playerctl lxappearance compton nitrogen network-manager-applet
+sudo dnf install i3-gaps polybar rofi playerctl lxappearance compton nitrogen network-manager-applet urvxt
+
+# grab me muh pywal (user only)
+pip3 install pywal --user
 
 # make some config directories
 mkdir ~/.config/polybar ~/.config/i3
