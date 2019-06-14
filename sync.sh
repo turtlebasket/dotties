c726a5b3d3243778ee4062075b6768bab3950643
turtlebasket <turtlebasket@gmail.com>
2019-06-13 23:52:58 -0700

    new configs & fedora script

--- sync.sh
+++
@@ -1,9 +0,0 @@
-cd configs/ 
-echo Copy Polybar config
-cp ~/.config/polybar/config polybarConfig
-echo Copy i3 config
-cp ~/.config/i3/config i3Config
-echo Copy Termite config
-cp ~/.config/termite/config termiteConfig
-echo Copy .bashrc
-cp ~/.bashrc bashrc
---
+++ universal-scripts/fedora-installation.sh
@@ -0,0 +1,4 @@
+# grab me some repos
+sudo dnf copr enable gregw/i3desktop mhartgring/polybar
+# grab me some essential packages
+sudo dnf install i3-gaps polybar rofi playerctl
