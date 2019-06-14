cd315cd33b29e6f733066a9d43a7a3949421e4e0
turtlebasket <turtlebasket@gmail.com>
2019-06-14 01:52:11 -0700

    more default packages

--- universal-scripts/fedora-installation.sh
+++ universal-scripts/fedora-installation.sh
@@ -1,4 +1,9 @@
 # grab me some repos
-sudo dnf copr enable gregw/i3desktop mhartgring/polybar
+sudo dnf copr enable gregw/i3desktop 
+sudo dnf copr enable mhartgring/polybar
+
 # grab me some essential packages
-sudo dnf install i3-gaps polybar rofi playerctl
+sudo dnf install i3-gaps polybar rofi playerctl lxappearance compton nitrogen network-manager-applet
+
+# make some config directories
+mkdir ~/.config/polybar ~/.config/i3
