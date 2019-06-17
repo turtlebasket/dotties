b2a140218b97fad7466c054fa64e0bb748b302b2
turtlebasket <turtlebasket@gmail.com>
2019-06-17 10:06:52 -0700

    added screenshot support

--- universal-scripts/fedora-installation.sh
+++ universal-scripts/fedora-installation.sh
@@ -3,10 +3,10 @@ sudo dnf copr enable gregw/i3desktop
 sudo dnf copr enable mhartgring/polybar
 
 # grab me some essential packages
-sudo dnf install i3-gaps polybar rofi playerctl lxappearance compton nitrogen network-manager-applet urvxt
+sudo dnf install i3-gaps polybar rofi playerctl lxappearance compton nitrogen network-manager-applet urvxt scrot xclip
 
 # grab me muh pywal (user only)
-pip3 install pywal --user
+pip3 install pywal neovim --user
 
 # make some config directories
 mkdir ~/.config/polybar ~/.config/i3
